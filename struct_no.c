e79467539f77e05bbdb32cecf85d2d50013b2b08
Guilherme Hinori <gui-benetti@hotmail.com>
2026-03-20 14:15:58 +0000

    struct

--- struct_no.c
+++ struct_no.c
@@ -22,12 +22,40 @@ void imprimir(struct No *head) {
     printf("NULL\n");
 }
 
+void removerInicio(struct No **head) {
+    if (*head == NULL) return; //lista vazio
+
+    struct No *temp = *head;
+    *head = (*head)->prox;
+    free(temp);
+}
+
+void inserirFim(struct No **head, int x) {
+    struct No *novo = (struct No*) malloc(sizeof(struct No));
+    novo->valor = x;
+    novo->prox = NULL; // será o ultimo nó
+
+    if(*head == NULL) {
+        *head = novo;
+    }
+    else {
+
+        struct No *aux = *head;
+        while (aux->prox != NULL) {
+            aux = aux->prox;
+        }
+        aux->prox = novo;
+    }
+}
+
 int main() {
     struct No *head = NULL; //Lista inicialmente vazia
 
-    inserirInicio(&head, 30);
-    inserirInicio(&head, 20);
-    inserirInicio(&head, 10);
-
+    inserirFim(&head, 10);
+    inserirFim(&head, 20);
+    inserirFim(&head, 30);
+    
     imprimir(head);
+
+    return 0;
 }
